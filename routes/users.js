ee457f194ef0e9354f4be3bd15fe6345f8cd73fc
VighneshVejandla2003 <143739641+VighneshVejandla@users.noreply.github.com>
2023-11-28 17:25:22 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
     res.send("hello user , you are logged in and you can delete your account")
 })
 
-router.get("/checkAdmin/:id",verifyAdmin,(req,res,next)=>{
+router.get("/isAdmin/:id",verifyAdmin,(req,res,next)=>{
     res.send("hello admin, you are logged in and you can delete all accounts")
 })
 
